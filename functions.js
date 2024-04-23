da079fecef8e21ac5e825d974b7bbed48e61b363
Adriath <adrianarjonabravo@gmail.com>
2024-04-23 19:19:26 +0200

    Terminadas validaciones del formulario de contacto, aunque faltaría comprobar el envío y qué ocurre tras el submit.
    Co-authored-by: Claudia Atkinson Bedia <ClaudiaAtkinsonBedia@users.noreply.github.com>

--- functions.js
+++ functions.js
@@ -9,15 +9,16 @@ function inicio() {
 
     let boton = document.getElementById("botonSubmit"); // Botón
     let nombre = document.getElementById("name"); // Nombre
-    let direccion = document.getElementById("direccion"); // Dirección
+    //let direccion = document.getElementById("direccion"); // Dirección
     let email = document.getElementById("email"); // Email
     let telefono = document.getElementById("telefono"); // Teléfono
     let mensaje = document.getElementById("mensaje");  // Mensaje
+    let form = document.getElementById("formContacto") ; // Formulario de contacto
 
     // MENSAJES DE ERRORES
 
     let errorNombre = document.getElementById("nameErrores"); // Nombre
-    let errorDireccion = document.getElementById("direccionErrores"); // Dirección
+    //let errorDireccion = document.getElementById("direccionErrores"); // Dirección
     let errorCorreo = document.getElementById("emailErrores"); // Email
     let errorTelefono = document.getElementById("telefonoErrores"); // Teléfono
     let errorMensaje = document.getElementById("mensajeErrores"); // Mensaje
@@ -38,7 +39,10 @@ function inicio() {
 
     mensaje.addEventListener("input", function () {
         cambiaColorInput(errorMensaje, validarMensaje());
-    }, false);
+    }, false) ;
+
+    form.addEventListener("submit", validarFormulario, false) ;
+
     
     // ------------------- FUNCIONES -----------------------------
 
@@ -48,7 +52,7 @@ function inicio() {
         function cambiaColorInput(elemento, esValido) {
 
         if (elemento.value == "") {
-            elemento.style.color = "gray";
+            elemento.style.color = "red";
         }
         else if (esValido) {
             elemento.style.color = "green";
@@ -83,27 +87,30 @@ function inicio() {
         let limiteCaracteres = /^.{1,50}$/;
     
         if (nombre.value.trim() === "") {
-            errorNombre.innerHTML = "El nombre es requerido" ;
+            errorNombre.innerHTML = "El nombre es requerido." ;
             nombre.focus();
             valido = false;
         }
         else if (!limiteCaracteres.test(nombre.value)) {
-            errorNombre.innerHTML = "El máximo son 50 caracteres";
+            errorNombre.innerHTML = "El máximo son 50 caracteres.";
             nombre.focus();
             valido = false;
         }
        else if(dobleEspacio.test(nombre.value))
             // Si se introducen dos espacios seguidos
         {
-            errorNombre.innerHTML = "No se permiten dos espacios seguidos" ;
+            errorNombre.innerHTML = "No se permiten dos espacios seguidos." ;
             nombre.focus() ;
 
             valido = false ;
             
         } 
         else if (!regNombre.test(nombre.value)) {
-            errorNombre.innerHTML = "El nombre no es válido. Asegúrese de que esté bien formado y no incluya guiones.";
+            errorNombre.innerHTML = "El nombre no es válido. <br> " +
+            "El nombre debe incluir nombre y apellido(s). <br>" + 
+            "Asegúrese de que esté bien escrito. Revise los caracteres especiales." ;
             nombre.focus();
+            
             valido = false;
         }
          else {
@@ -127,7 +134,7 @@ function inicio() {
         let regexC = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ ; // La expresión regular del correo electrónico
         if (email.value === "") // Si el campo del correo electrónico está vacío
         {
-            errorCorreo.innerHTML = "Debe introducir un email válido."; // Les indicamos en el div errorCorreo que escriban su correo
+            errorCorreo.innerHTML = "El correo electrónico es requerido."; // Les indicamos en el div errorCorreo que escriban su correo
             return false;
         }
         else if (!regexC.test(email.value)) // Si el correo electrónico que escriben no coincide con la expresión regular
@@ -156,12 +163,12 @@ function inicio() {
         let regexT = /^\d{9}$/;    
         if (telefono.value === "") // Si el campo del teléfono está vacío
         {
-            errorTelefono.innerHTML = "Debe introducir un teléfono válido."; // Les indicamos en el div errorTelefono que escriban su teléfono
+            errorTelefono.innerHTML = "El teléfono es requerido."; // Les indicamos en el div errorTelefono que escriban su teléfono
             return false;
         }
         else if (!regexT.test(telefono.value)) // Si el teléfono que escriben no coincide con la expresión regular
         {
-            errorTelefono.innerHTML = "El teléfono no cumple con los requisitos."; // Les indicamos en el div errorTelefono que escriban un teléfono que cumpla con los requisitos
+            errorTelefono.innerHTML = "El teléfono debe tener 9 dígitos."; // Les indicamos en el div errorTelefono que escriban un teléfono que cumpla con los requisitos
             return false;
         }
         else // En caso contrario, de que todo esté correcto y hayan escrito todo correcto
@@ -171,15 +178,23 @@ function inicio() {
         }
     }
 
+    /**
+    * Función que sirve para validar el mensaje del formulario.
+    * Requisitos:
+    *  - Debe de tener entre 20 y 500 caracteres.
+    *  - No puede estar vacío.
+    * 
+    * @returns {boolean} Devuelve true si el mensaje es correcto, false en caso contrario.
+    */
     function validarMensaje()
     {
         let valido = false ;
-        let limiteMinimoCaracteres = /^.{4,}$/ ; // Mínimo 20 caracteres y máximo 500.
-        let limiteMaximoCaracteres = /^.{0,20}$/;
+        let limiteMinimoCaracteres = /^.{20,}$/ ; // Mínimo 20 caracteres y máximo 500.
+        let limiteMaximoCaracteres = /^.{0,500}$/;
 
         if (mensaje.value.trim() === "") // Si está vacío
         {
-            errorMensaje.innerHTML = "El mensaje es requerido" ;
+            errorMensaje.innerHTML = "El mensaje es requerido." ;
             mensaje.focus();
             valido = false;
         }
@@ -187,25 +202,44 @@ function inicio() {
         {
             if (!limiteMinimoCaracteres.test(mensaje.value)) // Si se escriben caracteres por debajo del mínimo
             {
-                errorMensaje.innerHTML = "El mensaje debe tener más de 4 caracteres" ;
+                errorMensaje.innerHTML = "El mensaje debe tener un mínimo de 20 caracteres." ;
                 mensaje.focus() ;
                 valido = false ;
             }
-            else if(!limiteMaximoCaracteres.test(mensaje.value)) // Si se escriben caracteres por encima del máximo
-            { 
-                errorMensaje.innerHTML = "El mensaje debe tener menos de 20 caracteres" ;
+            else if (!limiteMaximoCaracteres.test(mensaje.value)) // Si se escriben caracteres por encima del máximo
+            {
+                let longitud = mensaje.value.length ;
+
+                errorMensaje.innerHTML = longitud + "/500 <br>" +
+                "El mensaje debe tener un máximo de 500 caracteres." ;
                 mensaje.focus() ;
                 valido = false ;
             }
             else
             {
-                errorMensaje.innerHTML = "Todo va bien" ;
+                let longitud = mensaje.value.length ;
+
+                errorMensaje.innerHTML = longitud + "/500" ;
                 valido = true ;
             }
         }
 
         return valido ;
-        /* errorMensaje.innerHTML = "";
-            valido = true; */
     }
+
+    function validarFormulario(e) {
+
+        let valido = false;
+
+        if (validarNombre() && validarEmail() && validarTelefono() && validarMensaje()) 
+        {
+            valido = true ;
+        }
+        else
+        {
+            e.preventDefault();
+        }
+    }
+
+
 }
