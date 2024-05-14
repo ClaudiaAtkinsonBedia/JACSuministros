24ab687a67b0cc9308f650f7dfbc89f42c7b53ee
Adriath <adrianarjonabravo@gmail.com>
2024-05-14 10:03:21 +0200

    Hemos creado la página de aviso legal y procedemos a su contenido.
    (Claudia y Adrián)

---
+++ VISTA/Secciones_web/main_avisoLegal.php
@@ -0,0 +1,109 @@
+
+<!-- MIGAS DE PAN -->
+
+<div class="container">
+    <div class="row">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.php?pagina=home"> Inicio </a></li>
+                <li class="breadcrumb-item active" aria-current="page"> Aviso Legal </li>
+            </ol>
+        </nav>
+    </div>
+</div>
+
+<!-- MAIN -->
+
+<main>
+    <div class="container">
+        <div class="row">
+
+            <h1 class="text-center"> Política de Cookies </h1>
+
+            <h2> Información sobre Cookies </h2>
+
+            <p> Conforme con la Ley 34/2002, de 11 de julio, de servicios de la sociedad de la información y de comercio
+                electrónico (LSSI), en relación con el Reglamento (UE)
+                2016/679 del Parlamento Europeo y del Consejo, de 27 de abril de 2016, General de Protección de Datos (GDPR) y
+                la Ley Orgánica 3/2018, de 5 de diciembre, de Protección de
+                Datos y Garantía de los Derechos Digitales (LOPDGDD), es obligado obtener el consentimiento expreso del
+                usuario de todas las páginas web que usan cookies prescindibles,
+                antes de que este navegue por ellas. </p>
+
+            <h4> ¿Qué son las Cookies? </h4>
+
+            <p> Las cookies y otras tecnologías similares tales como local shared objects, flash cookies o píxeles, son
+                herramientas empleadas por los servidores Web para
+                almacenar y recuperar información acerca de sus visitantes, así como para ofrecer un correcto funcionamiento
+                del sitio. <br>
+                <br>
+                Mediante el uso de estos dispositivos se permite al servidor Web recordar algunos datos concernientes al
+                usuario, como sus preferencias para la visualización
+                de las páginas de ese servidor, nombre y contraseña, productos que más le interesan, etc.
+            </p>
+
+            <h4> Cookies afectadas por la normativa y cookies exceptuadas </h4>
+
+            <p> Según la directiva de la UE, las cookies que requieren el consentimiento informado por parte del usuario son
+                las cookies de analítica y las de publicidad
+                y afiliación, quedando exceptuadas las de carácter técnico y las necesarias para el funcionamiento del sitio
+                web o la prestación de servicios
+                expresamente solicitados por el usuario. </p>
+
+            <h2> Tipos de Cookies </h2>
+
+            <h4> Según la finalidad </h4>
+            <p>
+                <span class="fw-bold"> <span class="fst-italic"> Cookies </span> técnicas y funcionales: </span> son aquellas que permiten al usuario la navegación a través de una página web,
+                plataforma o aplicación y la utilización de las diferentes opciones o servicios que en ella existan.
+                <br>
+                <span class="fw-bold"> <span class="fst-italic"> Cookies </span> analíticas: </span> son aquellas que permiten al responsable de las mismas el seguimiento y análisis del comportamiento
+                de los usuarios de los sitios web a los que están vinculadas. La información recogida mediante este tipo de cookies se
+                utiliza en la medición de la actividad de los sitios web, aplicación o plataforma y para la elaboración de perfiles de
+                navegación de los usuarios de dichos sitios, aplicaciones y plataformas, con el fin de introducir mejoras en función del
+                análisis de los datos de uso que hacen los usuarios del servicio.
+                <br>
+                <span class="fw-bold"><span class="fst-italic">Cookies</span> publicitarias:</span> son aquellas que permiten la gestión, de la forma más eficaz posible, de los espacios
+                publicitarios que, en su caso, el editor haya incluido en una página web, aplicación o plataforma desde la que presta el
+                servicio solicitado en base a criterios como el contenido editado o la frecuencia en la que se muestran los anuncios.
+                <br>
+                <span class="fw-bold"><span class="fst-italic">Cookies</span> de publicidad comportamental:</span> recogen información sobre las preferencias y elecciones personales del usuario
+                (retargeting) para permitir la gestión, de la forma más eficaz posible, de los espacios publicitarios que, en su caso,
+                el editor haya incluido en una página web, aplicación o plataforma desde la que presta el servicio solicitado.
+                <br>
+                <span class="fw-bold"> <span class="fst-italic"> Cookies </span> sociales: </span> son establecidas por las plataformas de redes sociales en los servicios para permitirle compartir
+                contenido con sus amigos y redes. Las plataformas de medios sociales tienen la
+                capacidad de rastrear su actividad en línea fuera de los Servicios. Esto puede afectar al contenido y los mensajes que
+                ve en otros servicios que visita.
+                <br>
+                <span class="fw-bold"><span class="fst-italic"> Cookies</span> de afiliados:</span> permiten hacer un seguimiento de las visitas procedentes de otras webs, con las que el sitio web
+                establece un contrato de afiliación (empresas de afiliación).
+                <br>
+                <span class="fw-bold"> <span class="fst-italic"> Cookies </span> de seguridad: </span> almacenan información cifrada para evitar que los datos guardados en ellas sean vulnerables a
+                ataques maliciosos de terceros.
+            </p>
+            <h4> Según la propiedad </h4>
+            <p>
+                <span class="fw-bold"> <span class="fst-italic"> Cookies </span> propias: </span> son aquellas que se envían al equipo terminal del usuario desde un equipo o dominio gestionado por el
+                propio editor y desde el que se presta el servicio solicitado por el usuario.
+                <br>
+                <span class="fw-bold"> <span class="fst-italic">Cookies</span> de terceros:</span> son aquellas que se envían al equipo terminal del usuario desde un equipo o dominio que no es
+                gestionado por el editor, sino por otra entidad que trata los datos obtenidos través de las cookies.
+            </p>
+            <h4> Según el plazo de conservación </h4>
+            <p>
+                <span class="fw-bold"> <span class="fst-italic">Cookies</span> de sesión:</span> son un tipo de cookies diseñadas para recabar y almacenar datos mientras el usuario accede a una página web.
+                <br>
+                <span class="fw-bold"> <span class="fst-italic"> Cookies </span> persistentes: </span> son un tipo de cookies en el que los datos siguen almacenados en el terminal y pueden ser
+                accedidos y tratados durante un período definido por el responsable de la cookie, y que puede ir de unos minutos a
+                varios años.
+            </p>
+            <h2> Tratamiento de datos personales </h2>
+            <p> REVISAR TEXTO </p>
+            <h2> Cookies utilizadas en este sitio </h2>
+            <p> MIRAR OTROS SITIOS Y LUEGO VER QUÉ HACEMOS CON ESTE APARTADO </p>
+
+        </div>
+
+    </div>
+</main>
