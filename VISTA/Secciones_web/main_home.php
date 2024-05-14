8a2ad702642d595d681890e87e81578ed25b0f1a
Adriath <adrianarjonabravo@gmail.com>
2024-05-14 11:35:38 +0200

    Empezamos modificaciones en el Home para añadir categorías en el main.
    Co-authored-by: Claudia Atkinson Bedia <ClaudiaAtkinsonBedia@users.noreply.github.com>

--- VISTA/Secciones_web/main_home.php
+++ VISTA/Secciones_web/main_home.php
@@ -6,8 +6,37 @@
   <!-- MAIN -->
   <main>
 
+  
 
     <div class="container mb-5">
+
+    <div class="row text-center mt-5">
+      <div class="col-4">
+        <a href="">
+          <img class="rounded-circle" src="VISTA/img/Categoria1.jpg" alt="Categoría 1" width="140" height="140">
+        </a>
+        <h2 class="fw-normal"> Categoría 1 </h2>
+        
+        
+      </div>
+      <div class="col-4">
+      <a href="">
+          <img class="rounded-circle" src="VISTA/img/Categoria2.webp" alt="Categoría 2" width="140" height="140">
+        </a>
+        <h2 class="fw-normal"> Categoría 2 </h2>
+        
+        
+      </div>
+      <div class="col-4">
+      <a href="">
+          <img class="rounded-circle" src="VISTA/img/Categoria3.webp" alt="Categoría 3" width="140" height="140">
+        </a>
+        <h2 class="fw-normal"> Categoría 3 </h2>
+        
+        
+      </div>
+    </div>
+
       <div class="row mt-4 ms-3">
         <p> <!-- Texto llamativo -->
           J.A.C SUMINISTROS INDUSTRIALES S.L., empresa fundada en 2002, formada por un grupo de profesionales con más de
