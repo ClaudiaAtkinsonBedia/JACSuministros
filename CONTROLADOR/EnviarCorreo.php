21a2acd1388ebeb9d361e85c5f43274ff25a540c
Adriath <adrianarjonabravo@gmail.com>
2024-05-17 14:11:07 +0200

    Pequeños arreglos en envío de correo: - Aparezcan correctamente los nombres de destinatario y remitente. - Arreglados caracteres especiales en el cuerpo.
    PROBLEMAS:
    - Caracteres especiales en el nombre.
    - En el email puede que también.
    - No sabemos cómo salir de lo que muestra al enviar.
    
    Co-authored-by: Claudia Atkinson Bedia <ClaudiaAtkinsonBedia@users.noreply.github.com>

--- CONTROLADOR/EnviarCorreo.php
+++ CONTROLADOR/EnviarCorreo.php
@@ -16,7 +16,7 @@ require_once '../PHPMailer/src/PHPMailer.php';
 require_once '../PHPMailer/src/SMTP.php';
 
 
-$metodo = $_SERVER['REQUEST_METHOD'] ;
+$metodo = $_SERVER['REQUEST_METHOD'];
 
 if ($metodo === 'POST') {
     $nombre = filter_var($_POST['name'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
@@ -27,63 +27,63 @@ if ($metodo === 'POST') {
 
     $mail = new PHPMailer(true);
 
-    
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.office365.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'adrianarjonabravo@hotmail.es';                     //SMTP username
-    $mail->Password   = 'aawjxkztkljodyxl';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    //Recipients
-    $mail->setFrom('adrianarjonabravo@hotmail.es', 'JAC Suministros');
-    $mail->addAddress('adrianarjonabravo@hotmail.es', 'Joe User');     //Add a recipient
-    // $mail->addAddress('ellen@example.com');               //Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.office365.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'adrianarjonabravo@hotmail.es';                     //SMTP username
+        $mail->Password   = 'aawjxkztkljodyxl';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    //Attachments
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+        //Recipients
+        $mail->setFrom('adrianarjonabravo@hotmail.es', $nombre);
+        $mail->addAddress('adrianarjonabravo@hotmail.es', 'JAC Suministros');     //Add a recipient
+        // $mail->addAddress('ellen@example.com');               //Name is optional
+        $mail->addReplyTo('info@example.com', 'JAC Suminstros');
+        // $mail->addCC('cc@example.com');
+        // $mail->addBCC('bcc@example.com');
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        //Attachments
+        // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Mensaje de ' . $nombre;
+        $mail->Body    = $mensaje;
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-//     $destinatario = 'adrian.arjona@abacodev.com';
-//     $asunto = 'Nuevo mensaje de contacto de ' . $nombre ;
-//     $cuerpo = "Nombre: $nombre\n";
-//     $cuerpo .= "Dirección: $direccion\n";
-//     $cuerpo .= "Email: $email\n";
-//     $cuerpo .= "Teléfono: $telefono\n";
-//     $cuerpo .= "Mensaje: $mensaje\n";
+        $mail->CharSet = 'UTF-8';
 
-//     $headers = "From: $email\r\n";
+        $mail->send();
+        // echo 'Message has been sent';
+        Header('Location: CONTROLADOR/Route.php') ;
 
-//     mail($destinatario, $asunto, $cuerpo, $headers);
-//     echo 'success';
-// }
-// else
-// {
-//     echo 'error';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+
+    //     $destinatario = 'adrian.arjona@abacodev.com';
+    //     $asunto = 'Nuevo mensaje de contacto de ' . $nombre ;
+    //     $cuerpo = "Nombre: $nombre\n";
+    //     $cuerpo .= "Dirección: $direccion\n";
+    //     $cuerpo .= "Email: $email\n";
+    //     $cuerpo .= "Teléfono: $telefono\n";
+    //     $cuerpo .= "Mensaje: $mensaje\n";
+
+    //     $headers = "From: $email\r\n";
+
+    //     mail($destinatario, $asunto, $cuerpo, $headers);
+    //     echo 'success';
+    // }
+    // else
+    // {
+    //     echo 'error';
 }
 
 
 // ENCRYPTION_SMTPS
-
-
-
-
